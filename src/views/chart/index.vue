780aa5491673c71805856f38ffd4772f3465b5cc
caoyong <>
2025-02-11 17:42:02 +0800

    修改bug

--- src/views/chart/index.vue
+++ src/views/chart/index.vue
@@ -43,11 +43,15 @@ const graphOptions = {
 		label: "自动布局",
 		layoutName: "force",
 		// from: "left",
-		layoutClassName: "seeks-layout-center",
+		// layoutClassName: "seeks-layout-center",
 		defaultExpandHolderPosition: "hide",
 		defaultJunctionPoint: "border",
 		force_node_repulsion: 2,
 		force_line_elastic: 0.5,
+		min_per_width: "300",
+		max_per_width: "300",
+		min_per_height: "40",
+		max_per_height: "60",
 	},
 }
 export default {
@@ -57,6 +61,9 @@ export default {
 			queryForm: {},
 			graphOptions,
 			nodes: [],
+			lines: [],
+			clickCount: 0, // 记录点击次数
+			clickTimer: null, // 用于延迟单击事件的定时器
 		}
 	},
 	mounted() {
@@ -75,17 +82,40 @@ export default {
 		//获取数据
 		async getQbRelation() {
 			const { data } = await getQbRelation(this.queryForm)
-			this.nodes = Object.freeze(data.nodes)
-			const nodes = data.nodes.map((item) => {
+
+			let nodes = data.nodes.map((item) => {
 				return {
 					...item,
 					color: colors[item.type],
 				}
 			})
+			this.nodes = Object.freeze(nodes)
+
+			let lines = data.lines
+			this.lines = Object.freeze(lines)
+
+			// 统计每个节点的连接次数
+			const nodeConnections = nodes.reduce((acc, node) => {
+				acc[node.id] = 0 // 初始化连接次数
+				return acc
+			}, {})
+
+			lines.forEach((line) => {
+				nodeConnections[line.from]++
+				nodeConnections[line.to]++
+			})
+
+			// 找出只有一条连接线的节点
+			const isolatedNodes = Object.keys(nodeConnections).filter((nodeId) => nodeConnections[nodeId] <= 5)
+
+			// 删除节点和与这些节点相关的连线
+			const filteredNodes = nodes.filter((node) => !isolatedNodes.includes(node.id))
+			const filteredLines = lines.filter((line) => !isolatedNodes.includes(line.from) && !isolatedNodes.includes(line.to))
+
 			const __graph_json_data = {
 				rootId: "2",
-				nodes,
-				lines: data.lines,
+				nodes: filteredNodes,
+				lines: filteredLines,
 			}
 			console.log("__graph_json_data:", __graph_json_data)
 
@@ -99,7 +129,26 @@ export default {
 			})
 		},
 		onNodeClick(nodeObject, $event) {
-			console.log("onNodeClick:", nodeObject)
+			this.clickCount++ // 点击次数加 1
+
+			if (this.clickCount === 1) {
+				// 第一次点击，设置定时器
+				this.clickTimer = setTimeout(() => {
+					this.onSingleClick(nodeObject) // 执行单击事件
+					this.clickCount = 0 // 重置点击次数
+					console.log("onSingleClick")
+				}, 300)
+			} else if (this.clickCount === 2) {
+				// 第二次点击，清除定时器并执行双击事件
+				clearTimeout(this.clickTimer)
+				this.onDoubleClick(nodeObject) // 执行双击事件
+				console.log("onDoubleClick")
+
+				this.clickCount = 0 // 重置点击次数
+			}
+		},
+		onSingleClick(nodeObject) {
+			// console.log("onNodeClick:", nodeObject)
 			if (nodeObject.type === "ZB") {
 				this.$refs.EquipmentViewRef.openDrawer(nodeObject.id)
 			}
@@ -109,6 +158,48 @@ export default {
 			if (nodeObject.type === "ZL") {
 				this.$router.push(`/home/war/${nodeObject.id}`)
 			}
+		},
+		onDoubleClick(nodeObject) {
+			// 找到当前节点
+			const node = this.nodes.find((n) => n.id === nodeObject.id)
+			if (!node) return
+
+			// 查找与该节点相连接的所有相关节点
+			const relatedNodesIds = new Set()
+			const relatedLines = this.lines.filter((line) => {
+				if (line.from === nodeObject.id) {
+					relatedNodesIds.add(line.to)
+				} else if (line.to === nodeObject.id) {
+					relatedNodesIds.add(line.from)
+				}
+				return line.from === nodeObject.id || line.to === nodeObject.id
+			})
+
+			// 获取所有相关节点
+			const relatedNodes = this.nodes.filter((n) => relatedNodesIds.has(n.id))
+
+			const __graph_json_data = {
+				rootId: "2",
+				nodes: relatedNodes,
+				lines: relatedLines,
+			}
+
+			const graphInstance = this.$refs.graphRef.getInstance()
+			console.log(graphInstance.getLinks())
+
+			graphInstance.addNodes(__graph_json_data.nodes)
+			graphInstance.addLines(__graph_json_data.lines)
+			graphInstance.refresh()
+			graphInstance.setZoom(30)
+
+			// console.time("render")
+			// const rgInstanceRef = this.$refs.graphRef
+			// rgInstanceRef.appendJsonData(__graph_json_data, (graphInstance) => {
+			// 	// 这些写上当图谱初始化完成后需要执行的代码.
+			// 	// 由于自动布局位置稳定下来需要时间，所以这里在1.8秒后重新让画面居中并缩放
+			// 	graphInstance.setZoom(30)
+			// 	console.timeEnd("render")
+			// })
 		},
 		onLineClick(lineObject, $event) {
 			console.log("onLineClick:", lineObject)
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
  * @Author: yc
  * @Date: 2024-11-23 20:22:35
  * @LastEditors: yc
- * @LastEditTime: 2025-02-10 19:51:00
+ * @LastEditTime: 2025-02-11 14:09:31
  * @Description: webpack配置
  */
 const path = require("path")
@@ -18,7 +18,7 @@ module.exports = {
 		compress: true, // 启用压缩，代码占用的空间少，在互联网传输快
 		proxy: {
 			"/api": {
-				target: "http://juihp9.natappfree.cc", //接口地址
+				target: "http://pe5vqd.natappfree.cc", //接口地址
 				changeOrigin: true, //允许跨域
 				pathRewrite: {
 					"^/api": "", //路径重写
