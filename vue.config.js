633fea4fe38420a6af7c37a06687c4c9d1c86828
ycxy12 <ycxy12@gmail.com>
2025-04-17 20:53:28 +0800

    修改地址

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
 		compress: true, // 启用压缩，代码占用的空间少，在互联网传输快
 		proxy: {
 			"/api": {
-				target: "http://29n6qx.natappfree.cc", //接口地址
+				target: "http://x4nmr7.natappfree.cc", //接口地址
 				changeOrigin: true, //允许跨域
 				pathRewrite: {
 					"^/api": "", //路径重写
