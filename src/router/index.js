df54ea70f600de4167749ed6c0f7173616d79fce
ycxy12 <ycxy12@gmail.com>
2025-06-06 22:06:45 +0800

    添加userId判断

--- src/router/index.js
+++ src/router/index.js
@@ -66,7 +66,7 @@ router.beforeEach(async (to, from, next) => {
 	if (ROUTER_WHITE_LIST.includes(to.path)) return next()
 
 	// 5.判断是否有 Token，没有重定向到 login 页面
-	if (!store.getters.getToken) return next({ path: "/login", replace: true })
+	if (!store.getters.getToken && !store.getters.getUserId) return next({ path: "/login", replace: true })
 
 	// 6.如果没有菜单列表，就重新请求菜单列表并添加动态路由
 	if (!store.getters.authMenuList.length) {
