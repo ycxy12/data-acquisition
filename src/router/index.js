03cac471e0178ae64927911e6552073a3d069636
ycxy12 <ycxy12@gmail.com>
2025-06-10 21:46:48 +0800

    修改路由地址

--- src/router/index.js
+++ src/router/index.js
@@ -52,8 +52,11 @@ router.beforeEach(async (to, from, next) => {
 	console.log(to.query.userId, 'router.beforeEach')
 	if(to.query.userId){
 		store.commit('setUserId',to.query.userId)
+	}
+	if(to.query.roleName){
 		store.commit('setRoleName',to.query.roleName)
 	}
+
 
 	// 3.判断是访问登陆页，有 Token 就在当前页面，没有 Token 重置路由到登陆页
 	if (to.path.toLocaleLowerCase() === "/login") {
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,21 @@
 <template>
 	<div>
-		<Tabs />
+		<Tabs v-if="!getUserId" />
 		<router-view></router-view>
 	</div>
 </template>
 
 <script>
 import Tabs from "./components/Tabs.vue"
+import { mapGetters } from "vuex"
 
 export default {
 	components: { Tabs },
+	computed: {
+		...mapGetters(["getUserId", "getRoleName"]),
+	},
+	mounted() {
+		console.log(this.getUserId, "getUserId")
+	},
 }
 </script>
